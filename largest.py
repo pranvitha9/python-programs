6c927604471dc7a0d337365163cb7cfa42118613
pranvitha9 <pranvithapoolakunta@gmail.com>
2026-05-13 20:08:26 +0530

    Add files via upload

---
+++ largest.py
@@ -0,0 +1,4 @@
+numbers=[1,45,67,23,89,34]
+largest=max(numbers)
+print("List:",numbers)
+print("Largest Element:",largest)
---
+++ smallest.py
@@ -0,0 +1,4 @@
+numbers=[12,45,67,23,89,34]
+smallest=min(numbers)
+print("List:",numbers)
+print("smallest Element:",smallest)
